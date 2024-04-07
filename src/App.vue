cd331cf3e9befb51a1c633dc99350f07083deb5f
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-04-07 14:13:13 +0100

    made a counter, it can start and can stop

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const timer = ref(4);
+let timeInterval;
+
+function start() {
+  if (timeInterval) return;
+
+  timeInterval = setInterval(() => {
+    timer.value--;
+
+    if (timer.value === 0) {
+      stop();
+    }
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(timeInterval);
+}
+</script>
 <template>
   <button>Pomodoro</button>
   <button>Long Break</button>
   <button>Short Break</button>
-  <div>60</div>
-  <button>Start</button>
-  <button>Pause</button>
+  <div>{{ timer }}</div>
+  <button @click="start">Start</button>
+  <button @click="stop">Pause</button>
   <button>Restart</button>
   <button>Config</button>
 </template>
