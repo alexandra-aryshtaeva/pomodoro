e73d022c3b634e0f8a1e7daa66416f69f7cf9820
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-04-07 13:35:24 +0100

    basic html

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+  <button>Pomodoro</button>
+  <button>Long Break</button>
+  <button>Short Break</button>
+  <div>60</div>
+  <button>Start</button>
+  <button>Pause</button>
+  <button>Restart</button>
+  <button>Config</button>
+</template>
+
+<style>
+body {
+  font-size: 32px;
+}
+</style>
